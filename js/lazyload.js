340c846c7b3767cdfec16343a338d7dbc358196d
Mayank Raj <AgrMayank@outlook.com>
2025-01-13 00:21:35 +0530

    Add YouTube Lazy Loading for massive speed boost

---
+++ js/lazyload.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyloadElements = document.querySelectorAll(".lazyload");
+    var observer;
+
+    function loadIframe(iframe) {
+        iframe.src = iframe.getAttribute("data-src");
+        iframe.classList.remove("lazyload");
+    }
+
+    if ("IntersectionObserver" in window) {
+        observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    loadIframe(entry.target);
+                    observer.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyloadElements.forEach(function (iframe) {
+            observer.observe(iframe);
+        });
+    } else {
+        // Fallback for older browsers
+        var throttleTimeout;
+
+        function lazyload() {
+            if (throttleTimeout) {
+                clearTimeout(throttleTimeout);
+            }
+
+            throttleTimeout = setTimeout(function () {
+                var scrollTop = window.pageYOffset;
+                lazyloadElements.forEach(function (iframe) {
+                    if (iframe.offsetTop < window.innerHeight + scrollTop) {
+                        loadIframe(iframe);
+                    }
+                });
+                if (lazyloadElements.length === 0) {
+                    removeListeners();
+                }
+            }, 200);
+        }
+
+        function removeListeners() {
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+        }
+
+        document.addEventListener("scroll", lazyload);
+        window.addEventListener("resize", lazyload);
+        window.addEventListener("orientationChange", lazyload);
+    }
+});
